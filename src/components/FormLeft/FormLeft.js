fab114f6f6aab331f0bb8420d229be77248199f7
hemanta4291 <hemanta25roy@gmail.com>
2021-07-15 01:44:57 +0600

    done

--- src/components/FormLeft/FormLeft.js
+++ src/components/FormLeft/FormLeft.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import logo from '../../assets/images/whitelogo.png'
+// import logoback from '../../assets/images/logoBack.png'
  const FromLeft= ()=> {
 
     return (
         <div className="signup__left--content">
+            {/* <div className="logo__back">
+                <img src={logoback} alt="" />
+            </div> */}
             <img src={logo} alt="" />
             <h3>Review Your Bussiness</h3>
             <p>Keep your reputation intact with Business Review Central. Create your 
